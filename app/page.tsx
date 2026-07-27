04375c9ffbc5d63654b8ae62503f16291bfc413b
FacundoVillarroel <facu.villarroel96@gmail.com>
2026-07-28 00:23:28 +0200

    Update CV

--- app/page.tsx
+++ app/page.tsx
@@ -729,7 +729,7 @@ export default function Portfolio() {
         <footer className="border-t bg-white dark:bg-slate-950 dark:border-slate-800">
           <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0 px-8">
             <div className="text-center text-sm leading-loose text-slate-500 dark:text-slate-400 md:text-left">
-              © 2025 Facundo Villarroel. All rights reserved.
+              © 2026 Facundo Villarroel. All rights reserved.
             </div>
             <div className="flex items-center gap-4">
               <Link
